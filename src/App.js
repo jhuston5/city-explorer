38dcfa94951fa54ff6708173f5ba8896b74dd7c2
Joshua Huston <joshua.david.huston@gmail.com>
2021-10-06 23:55:52 -0400

    Added barebones location feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cityName: '',
+      locationObj: {}
+    }
+    //functions
+  }
+
+  getLocation = async (event) => {
+    event.preventDefault()
+    console.log('button pushed', this.state.cityName);
+    
+    console.log(process.env.REACT_APP_LOCATION_API_KEY);
+    let URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${this.state.cityName}&format=json`;
+    console.log(URL);
+
+    try {
+      let locData = await axios.get(URL);
+      //Response obj.data
+      console.log(locData.data[0])
+      //Set it to setate
+      this.setState({
+        locationObj:
+          locData.data[0]
+      });
+      console.log(this.state.locationObj);
+    }
+    //If there is an error in the try
+    catch (error) {
+      console.log('there was an error:', error);
+    }
+
+  }
+  
+
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Enter A City!</h1>
+      
+      
+      <form>
+        <fieldset>
+          <label>City</label> <input onChange={(e) => {this.setState({ cityName: e.target.value })}} type="text" id="city" name="city" placeholder="Your City"></input>
+        </fieldset>
+        <button type="submit" 
+          onClick={this.getLocation}
+        >Explore!</button>
+      </form>
+      <div>
+        <h3>{this.state.cityName}</h3>
+        <p>Latitude: {this.state.locationObj.lat}</p>
+        <p>Longitude: {this.state.locationObj.lon}</p>
+      </div>
+    </>
   );
+}
 }
 
 export default App;
